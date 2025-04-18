079a4884352506cb43ace706e781a4e41835f522
GiovanaGomessz <giovanavgf2008@gmail.com>
2025-04-17 21:41:13 -0400

    atualizando 17.04.25

---
+++ view/components/nav.php
@@ -0,0 +1,8 @@
+<header>
+    <nav>
+        <ul>
+            <li><a href="playlist.php">Músicas</a></li>
+            <li><a href="artigos.php">Artigos</a></li>
+        </ul>
+    </nav>
+</header>
---
+++ view/pages/artigos.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Artigos</title>
+
+    <link rel="stylesheet" href="../assets/css/artigos.css">
+</head>
+
+<body>
+    <?php require_once '../components/nav.php'; ?>
+
+    <main>
+        <h1>Artigos</h1>
+    </main>
+</body>
+
+</html>
