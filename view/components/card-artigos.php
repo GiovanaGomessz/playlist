4ba254aabcbbd73c64172a7827a65b58b24f700c
GiovanaGomessz <giovanavgf2008@gmail.com>
2025-04-25 21:42:20 -0400

    atualizando css do arquivo

--- view/components/card-artigos.php
+++ view/components/card-artigos.php
@@ -1,12 +1,14 @@
 <?php
-function renderizarCard($titulo, $descricao, $imagem)
+function renderizarCard($imagem, $titulo, $descricao, $link )
 {
     return "
     <div class='card'>
         <img src=$imagem>
-        <h3>$titulo</h3>
-        <p>$descricao</p>  
-        
+        <div class='organizacao'>
+            <h3>$titulo</h3>
+            <p>$descricao</p>  
+        </div>       
+        <a class='saiba_mais' href='$link'>Saiba Mais</a> 
     </div>
     ";
 }
--- view/pages/artigos.php
+++ view/pages/artigos.php
@@ -20,27 +20,72 @@ require_once '../components/card-artigos.php';
         <div class="container">
             <?php
             echo renderizarCard(
-                "BLABLABLA",
-                "ASDFNBVDJKSLDKJFHGJFKDLKFVJBHJVKCLXKCVSXDCFGHJMK",
                 "https://i1.sndcdn.com/artworks-TIMKMozp6RBoEyHw-5zOpTQ-t500x500.jpg",
-
+                "BK",
+                "Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat.
+                In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor.
+                Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere.
+                Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos. 
+                Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat.
+                In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor.
+                Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere.",
+                "",
             );
 
             echo renderizarCard
             (
-                "Luisa Sonza",
-                "Bêbada favorita",
                 "https://s2-g1.glbimg.com/PdfwBwbI2SO9O3f8ZyKjZKFPUp8=/0x0:2048x2048/1008x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2023/q/q/A40hpHRceiB3Ewik8t2w/luisasonzaescandalointimocapa.jpg",
-
+                "Luisa Sonza",
+                "Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat.
+                In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor.
+                Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere.
+                Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos. 
+                Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat.
+                In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor.
+                Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere.",
+                "",
             );
 
             echo renderizarCard
             (
-                "Baco Exu do Blues",
-                "Te amo Desgraça",
                 "https://akamai.sscdn.co/uploadfile/letras/albuns/8/5/0/a/595141505149846.jpg",
+                "Baco Exu do Blues",
+                "Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat.
+                In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor.
+                Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere.
+                Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos. 
+                Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat.
+                In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor.
+                Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere.",
+                "",
+            );
 
+            echo renderizarCard
+            (
+                "https://cdn-images.dzcdn.net/images/cover/000d9948b14c8c4b20653d017dfa5079/200x200.jpg",
+                "Carol Biazin",
+                "Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat.
+                In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor.
+                Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere.
+                Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos. 
+                Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat.
+                In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor.
+                Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere.",
+                "",
+            );
 
+            echo renderizarCard
+            (
+                "https://i.scdn.co/image/ab67616d00001e02530daaf54e428e5d267e6cac",
+                "Os Garotin",
+                "Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat.
+                In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor.
+                Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere.
+                Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos. 
+                Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat.
+                In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor.
+                Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere.",
+                "",
             );
 
             ?>
